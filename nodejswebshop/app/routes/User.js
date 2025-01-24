148881705c6278cd9861f559a79f91af081fd777
YOSEF NADEMO <72689921+Josefnademo@users.noreply.github.com>
2025-01-24 12:08:35 +0100

    24.01.2025
    Des détails sont présents dans JDT

--- nodejswebshop/app/routes/User.js
+++ nodejswebshop/app/routes/User.js
@@ -1,6 +1,23 @@
-const express = require('express');
+const express = require("express");
 
 const router = express.Router();
 const controller = require("../controllers/UserController");
-router.get('/', controller.get) 
-module.exports = router; 
+//router.get("/", controller.get);
+module.exports = router;
+router.get("/login", (req, res) => {
+  res.render("login", { name: "Yosef" });
+  if (!res.render("login")) {
+    res.status(404).send("page introuvable. Veuillez verifier votre URL");
+  }
+});
+
+/*app.get("/menu", (req, res) => {
+  res.render("menu");
+});*/
+
+/*app.get("/login", (req, res) => {
+  res.render("login");
+  if (!res.render("login")) {
+    res.status(404).send("page introuvable. Veuillez verifier votre URL");
+  }
+});*/
--- nodejswebshop/app/server.js
+++ nodejswebshop/app/server.js
@@ -1,13 +1,21 @@
 const express = require("express");
-
-
 const app = express();
-const userRoute = require('./routes/User');
-app.use('/user', userRoute);
+const userRoute = require("./routes/User");
+/*const path = require("path");*/
 
+app.use("/user", userRoute);
 
+app.set("view engine", "ejs");
 
-// Démarrage du serveur
+/*// Serve static files (CSS, JS, images)
+app.use(express.static(path.join(__dirname, "public")));*/
+
+// Other routes and middleware
+app.get("/", (req, res) => {
+  res.render("login", { name: "Yosef" });
+});
+
+// server butting
 app.listen(8080, () => {
-    console.log('Server running on port 8080');
+  console.log("Server running on port 8080");
 });
